8a188b272a0d560060b3ddd83f88cfcfc9db222a
Deepak <deepakkothandaraman07@gmail.com>
2025-10-08 20:38:03 +0530

    study of logic gates

---
+++ simulation/qsim/work/@exp1/_primary.vhd
@@ -0,0 +1,14 @@
+library verilog;
+use verilog.vl_types.all;
+entity Exp1 is
+    port(
+        a               : in     vl_logic;
+        b               : in     vl_logic;
+        c               : out    vl_logic;
+        d               : out    vl_logic;
+        e               : out    vl_logic;
+        f               : out    vl_logic;
+        g               : out    vl_logic;
+        h               : out    vl_logic
+    );
+end Exp1;
---
+++ simulation/qsim/work/@exp1_vlg_check_tst/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity Exp1_vlg_check_tst is
+    port(
+        c               : in     vl_logic;
+        d               : in     vl_logic;
+        e               : in     vl_logic;
+        f               : in     vl_logic;
+        g               : in     vl_logic;
+        h               : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end Exp1_vlg_check_tst;
